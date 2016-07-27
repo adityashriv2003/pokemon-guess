67b2332b8110ec6a991fbac50434c9dc7b5c5da5
adityashriv2003 <adityashrivastava2003@gmail.com>
2016-07-28 00:17:07 +0530

    Add files via upload

---
+++ pokemon go.py
@@ -0,0 +1,9 @@
+print("pokemon go")
+ash_pokemon = input("Guess the pokemon=:::::::::::::::::::::::::::::::::::::")
+if ash_pokemon == "pikachu" or ash_pokemon == "charizard" or ash_pokemon == "bulbasaur" or ash_pokemon == "squirtil":
+    print("you won them")
+else:
+    print("you losed them")
+print("pokemen over")
+input()
+
