51501b7035cdf9f2da40b40e286658ce5c5345b4
euler16 <nilayshrivastava1729@gmail.com>
2016-10-23 11:01:24 +0530

    changes

--- pokemon go.py
+++ pokemon go.py
@@ -1,9 +1,11 @@
+#MY FIRST GAME - POKEMON GUESS!!!!!!!!
+
 print("pokemon go")
-ash_pokemon = input("Guess the pokemon=:::::::::::::::::::::::::::::::::::::")
+ash_pokemon = input("::::::::::::::::::::::::::::Guess the pokemon=:::::::::::::::::::::::::::::::::::::")
 if ash_pokemon == "pikachu" or ash_pokemon == "charizard" or ash_pokemon == "bulbasaur" or ash_pokemon == "squirtil":
     print("you won them")
 else:
     print("you losed them")
 print("pokemen over")
-input()
+
 
